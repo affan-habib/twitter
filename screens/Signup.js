d143fc57fc6a5be414645479af52e0eaf9fa6ef3
affan-habib <affan.habib44@gmail.com>
2023-04-04 12:32:07 +0600

    twitter clone init

---
+++ screens/Signup.js
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+
+const SignupSchema = yup.object().shape({
+  username: yup.string().required("username is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+});
+
+const TwitterSignupScreen = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSignup = async (values) => {
+    try {
+      const response = await axios.post(
+        "https://missingdata.pythonanywhere.com/signup",
+        {
+          username: values.username,
+          email: values.email,
+          password: values.password,
+        }
+      );
+      console.log(response.data);
+      // handle success here
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("There was an error signing up. Please try again.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/twitter.png")} />
+      <Text style={styles.title}>Create your account</Text>
+      {errorMessage !== "" && <Text style={styles.error}>{errorMessage}</Text>}
+      <Formik
+        initialValues={{ username: "", email: "", password: "" }}
+        onSubmit={(values) => handleSignup(values)}
+        validationSchema={SignupSchema}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View style={styles.form}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={[
+                  styles.input,
+                  touched.username && errors.username
+                    ? styles.inputError
+                    : null,
+                ]}
+                placeholder="username"
+                onChangeText={handleChange("username")}
+                onBlur={handleBlur("username")}
+                value={values.username}
+              />
+              {touched.username && errors.username ? (
+                <Text style={styles.errorText}>{errors.username}</Text>
+              ) : null}
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={[
+                  styles.input,
+                  touched.email && errors.email ? styles.inputError : null,
+                ]}
+                placeholder="Email"
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                keyboardType="email-address"
+              />
+              {touched.email && errors.email ? (
+                <Text style={styles.errorText}>{errors.email}</Text>
+              ) : null}
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={[
+                  styles.input,
+                  touched.password && errors.password
+                    ? styles.inputError
+                    : null,
+                ]}
+                placeholder="Password"
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                secureTextEntry
+              />
+              {touched.password && errors.password ? (
+                <Text style={styles.errorText}>{errors.password}</Text>
+              ) : null}
+            </View>
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  form: {
+    width: "100%",
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 16,
+  },
+  inputError: {
+    borderColor: "red",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+  },
+  button: {
+    backgroundColor: "#1DA1F2",
+    paddingVertical: 15,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  error: {
+    color: "red",
+    marginBottom: 20,
+  },
+});
+
+export default TwitterSignupScreen;
