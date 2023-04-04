98729d0302ffe01217840f85d4caec037f83da37
affan-habib <affan.habib44@gmail.com>
2023-04-04 15:21:14 +0600

    updated login and logut screens and navigation

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import TwitterSignupScreen from "./screens/Signup";
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AsyncStorage } from 'react-native';
+import TwitterSignupScreen from './screens/Signup';
+import TwitterLoginScreen from './screens/Login';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+    const token = await AsyncStorage.getItem('jwt_token');
+    setLoggedIn(token !== null);
+  };
+
   return (
-    <View style={styles.container}>
-      <TwitterSignupScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {loggedIn ? (
+          <Stack.Screen name="Dashboard" component={TwitterSignupScreen} />
+        ) : (
+          <Stack.Screen name="Login" component={TwitterSignupScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
